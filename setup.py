518c18c6774742d095c31d26b09c94cb55fbbca3
StrannikMC <strannik1295@gmail.com>
2014-06-24 20:52:17 +0300

    First

---
+++ setup.py
@@ -0,0 +1,13 @@
+﻿from cx_Freeze import setup, Executable
+ 
+exe = Executable(
+    script="time.py",
+    base="Win32GUI",
+    )
+   
+setup(
+    name = "wxSampleApp",
+    version = "0.1",
+    description = "An example wxPython script",
+    executables = [exe]
+    )
